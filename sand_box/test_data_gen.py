5f2681355301e7342aa50bad83a256d84d601538
nonieA <nonie.alexander.16@ucl.ac.uk>
2020-03-02 19:09:28 +0000

    wrote a data generation function

---
+++ sand_box/test_data_gen.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import os
+import numpy as np
+import random
+from sklearn.datasets import make_classification
+import seaborn as sns
+import timeit
+
+# testing quickest method to generate data
+k = [2,4,5]
+n = [100,500,600,700]
+feat = [14,53,10]
+noise = [0.3,0.05,0.1]
+
+sep = [0.1,0.5,1]
+split =[np.random.uniform(0,1/i,(i-1)) for i in k]
+
+# super nested lists
+
+
+[make_classification(n_samples = num,
+                     n_features= f,
+                     n_informative = f - int(round(f * noi,0)),
+                     n_redundant= int(round(f * noi,0)),
+                     n_classes = c,
+                     n_clusters_per_class = 1,
+                     class_sep= s)
+ for num in n for f in feat for noi in noise for c in k for s in sep]
+
+
+def many_data(k,n,feat,noise,sep,split,seed):
+    np.random.seed(seed)
+    data = [make_classification(n_samples=num,
+                         n_features=f,
+                         n_informative=f - int(round(f * noi, 0)),
+                         n_redundant=int(round(f * noi, 0)),
+                         n_classes=c,
+                         n_clusters_per_class=1,
+                         class_sep=s)
+            for num in n for f in feat for noi in noise for c in k for s in sep]
+    return(data)
+
+
+
